857999fdbd22743525d13dcec9823b8fc312413c
Andrew Park <apark025@usc.edu>
2024-04-10 04:45:00 +0000

    finished hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,9 +21,49 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
-
+bool findSchedule(size_t dayIndex, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, map<Worker_T, size_t>& shiftsCounter);
+bool tryScheduleDay(size_t dayIndex, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, map<Worker_T, size_t>& shiftsCounter, vector<Worker_T>& todaysCandidates, size_t startID, size_t workersAdded);
 // Add your implementation of schedule() and other helper functions here
+bool findSchedule(size_t dayIndex, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, map<Worker_T, size_t>& shiftsCounter){
+    if(dayIndex == avail.size()){ //base case: all days have been successfully scheduled
+        return true;
+    }
+
+    vector<Worker_T> todaysCandidates;
+    
+    for(size_t worker = 0; worker < avail[dayIndex].size(); ++worker){
+        if(avail[dayIndex][worker]){
+            todaysCandidates.push_back(worker);
+        }
+    }
+    //try to find valid combo of workers for today
+    return tryScheduleDay(dayIndex, avail, dailyNeed, maxShifts, sched, shiftsCounter, todaysCandidates, 0, 0);
+}
+
+
+bool tryScheduleDay(size_t dayIndex, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, map<Worker_T, size_t>& shiftsCounter, vector<Worker_T>& todaysCandidates, size_t startID, size_t workersAdded){
+    if(workersAdded == dailyNeed){ //base case: enough workers added for today
+        return findSchedule(dayIndex + 1, avail, dailyNeed, maxShifts, sched, shiftsCounter);
+    }
+
+    for(size_t i = startID; i < todaysCandidates.size(); ++i){
+        Worker_T worker = todaysCandidates[i];
+        //add worker if available and still under max shifts
+        if(avail[dayIndex][worker] && shiftsCounter[worker] < maxShifts){
+            sched[dayIndex].push_back(worker);
+            shiftsCounter[worker]++;
+            if(tryScheduleDay(dayIndex, avail, dailyNeed, maxShifts, sched, shiftsCounter, todaysCandidates, i+1, workersAdded+1)){
+                return true;
+            }
+            //backtrack
+            shiftsCounter[worker]--;
+            sched[dayIndex].pop_back();
+        }
+    }
+
+    return false; //no valid schedule found for this day
+}
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,9 +77,17 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    sched.resize(avail.size());
+    
+    map<Worker_T, size_t> shiftsCounter;
+
+    for(size_t day = 0; day < avail.size(); ++day){
+        for(Worker_T worker = 0; worker < avail[day].size(); ++worker){
+            shiftsCounter[worker] = 0;
+        }
+    }
 
 
-
-
+    return findSchedule(0, avail, dailyNeed, maxShifts, sched, shiftsCounter);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void generateWords(string currentWord, const string& in, const string& floating, set<string>& validWords, const set<string>& dict, size_t currentIndex, int dashCount);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,52 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> validWords;
 
+    int dashCount = 0;
+    for(size_t i = 0; i < in.size(); ++i){ //count blank spaces
+        if(in[i] == '-'){
+            dashCount++;
+        }
+    }
+
+    generateWords("", in, floating, validWords, dict, 0, dashCount);
+    return validWords;
 }
 
 // Define any helper functions here
+void generateWords(string currentWord, const string& in, const string& floating, set<string>& validWords, const set<string>& dict, size_t currentIndex, int dashCount){
+    if(currentIndex == in.length()){ //if word is complete and is in the dictionary
+        if(dict.find(currentWord) != dict.end()){
+            validWords.insert(currentWord);
+        }
+        return;
+    }
+    
+    if(in[currentIndex] != '-'){ //not a dash
+        generateWords(currentWord + in[currentIndex], in, floating, validWords, dict, currentIndex + 1, dashCount);
+    }
+    else{ //if blank
+        string tempFloating = floating;
+        for(size_t i = 0; i < tempFloating.size(); ++i){ //first try replacing with floating letter
+            char letter = tempFloating[i];
+            size_t pos = tempFloating.find(letter);
+            if(pos != string::npos){
+                tempFloating.erase(pos, 1); //erase the used floating letter
+            }
+            generateWords(currentWord + letter, in, tempFloating, validWords, dict, currentIndex + 1, dashCount-1);
+            tempFloating = floating; //reset for next iteration
+        }
+
+        if(dashCount > static_cast<int>(tempFloating.length())){ //if more letters are needed
+            for(char letter = 'a'; letter <= 'z'; ++letter){
+                tempFloating = floating; //reset
+                size_t i = tempFloating.find(letter);
+                if(i != string::npos){
+                    tempFloating.erase(i,1); //erase used floating letter
+                }
+                generateWords(currentWord + letter, in, tempFloating, validWords, dict, currentIndex + 1, dashCount - 1);
+            }
+        }
+    }
+}
